fc71ca219053e01ddc3bc6e93753e9d0c8cbcdb4
sei0jieai <35719255+sei0jieai@users.noreply.github.com>
2019-08-30 17:44:44 +0800

    Update dep_pro.sh

--- dep_pro.sh
+++ dep_pro.sh
@@ -9,7 +9,7 @@ shell_name="dep_pro.sh"
 time=$(date '+%Y%m%d-%H%M%S')
 log_file="/root/shell_deploy/logs/${shell_name}.log"
 dep_pack_path="/root/"
-dep_pack_name="web.war"
+dep_pack_name="apache-tomcat-8.5.43-devopstest.war"
 #工程目录-列表
 #tomcat目录
 pro_name="apache-tomcat-8.5.43-devopstest"
@@ -20,11 +20,11 @@ config_file="/test-dev/config_file/${pro_name}/*.conf"
 backup_path="/data/backup/${pro_name}"
 
 #部署目录(工程目录)
-zip_pack_name="web.war"
-deploy_path="/test-dev/$pro_name/ROOT"
+zip_pack_name="apache-tomcat-8.5.43-devopstest.war"
+deploy_path="/test-dev"
 
 #回滚目录（上一次备份目录）
-rollback_path="/test-dev/rollback/$pro_name/ROOT"
+rollback_path="/test-dev/rollback"
 
 function shell_log()
 {
@@ -35,19 +35,10 @@ function shell_log()
 function backup()
 {
     local backup_filename="${pro_name}_bak_${time}.tar.gz"
-    if [ -f ${backup_filename} ]; then 
-        touch ${backup_filename} 
-    fi
-    #备份回滚目录到备份目录
-    tar czf ${backup_filename} ${rollback_path} && mv ${backup_filename} ${backup_path}/${pro_name}/  && rm -rf ${rollback_path}/* && shell_log "backup rollback path OK!"
-    
-    #备份部署目录到回滚目录
-    
-    mv ${deploy_path}/* ${rollback_path}/
-    #解压新版本包到部署目录
-    
-    unzip -d ${deploy_path} /root/${zip_pack_name} && shell_log "unzip pack sucess" 
-    #重启服务
+    #备份部署目录到备份目录
+    mv ${deploy_path}/${pro_name} ${backup_path}/${pro_name}_bak_${time}
+    #解压新包到部署目录
+    mkdir ${dedeploy_path}/${pro_name} && unzip /root/${zip_pack_name} -d ${dedeploy_path}/${pro_name}
 }
 
 

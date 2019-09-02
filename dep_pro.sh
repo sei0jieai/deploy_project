00c513b0b282e2c7fb9c059c0f41f04c825fadbf
sei0jieai <35719255+sei0jieai@users.noreply.github.com>
2019-09-02 15:40:02 +0800

    Update dep_pro.sh

--- dep_pro.sh
+++ dep_pro.sh
@@ -9,7 +9,7 @@ shell_name="dep_pro.sh"
 time=$(date '+%Y%m%d-%H%M%S')
 log_file="/root/shell_deploy/logs/${shell_name}.log"
 dep_pack_path="/root/"
-dep_pack_name="apache-tomcat-8.5.43-devopstest.war"
+dep_pro_name="apache-tomcat-8.5.43-devopstest.war"
 pro_pid=`ps -ef|grep $pro_name|grep -v 'grep'|awk -F " " '{print $2}'`
 #工程目录-列表
 #tomcat目录
@@ -22,7 +22,7 @@ config_file_path="/test-dev/config_file/${pro_name}/"
 backup_path="/data/backup/${pro_name}"
 
 #部署目录(工程目录)
-zip_pack_name="apache-tomcat-8.5.43-devopstest.war"
+zip_pro_name="apache-tomcat-8.5.43-devopstest.war"
 deploy_path="/test-dev"
 
 #回滚目录（上一次备份目录）
@@ -44,7 +44,7 @@ function backup()
         [ $? -eq 0 ] && shell_log "move to ${backup_path} success." || { shell_log "move to ${backup_path} failed."; exit 1; }
     fi
     #解压新包到部署目录
-    mkdir ${deploy_path}/${pro_name} && unzip /root/${zip_pack_name} -d ${deploy_path}/${pro_name}
+    mkdir ${deploy_path}/${pro_name} && unzip /root/${zip_pro_name} -d ${deploy_path}/${pro_name}
 }
 
 
@@ -59,6 +59,21 @@ function add_conf()
     fi
 }
 
+function stop_pro()
+{
+    pro_num=$(ps -ef | grep "${pro_name}/conf" | grep -v "grep" | wc -l)
+    if [ "$pro_num" -eq "1" ];then
+        local pro_pid=$(ps -ef | grep "${pro_name}/conf" | grep -v "grep" | awk -F " " '{print $2}')
+        echo -e "jstat status:\n$(jstat -gcutil $pro_pid)" >> ${log_file}
+        kill -9 ${pro_pid}
+        [ $? -eq 0 ] && shell_log "kill ${pro_pid} process success." || { shell_log "kill ${pro_pid} process failed, exit!"; exit 1; }
+    elif [ "$pro_num" -eq "0" ];then
+        shell_log "warning: $pro_num ${pro_name} running!"
+    else
+        shell_log "warning: $pro_num ${pro_name} running! exit."
+        exit 1
+    fi
+}
 
 
 
@@ -72,6 +87,9 @@ function deploy()
             ;;
         add_conf)
             add_conf
+            ;;
+        stop_pro)
+            stop_pro
             ;;
     esac
 }
